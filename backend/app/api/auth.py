9e5c68eb202e8e7e2ceee78a42e2c9729d61e83c
Utilisateur Lambda <clement.chassem@gmail.com>
2025-06-17 15:53:31 +0200

    minor fix

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -7,7 +7,7 @@ from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
 from slowapi import Limiter, _rate_limit_exceeded_handler
-from slowapi import get_remote_address
+from slowapi.util import get_remote_address
 
 limiter = Limiter(key_func=get_remote_address)
 
